3e95a14d23d70040135e240637e03450a34c4056
Deja Trudeaux <dejadej@gmail.com>
2018-11-08 15:09:15 -0600

    calendar example data rendering

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,43 +1,254 @@
 <template>
-  <div id="calendar">
-		<h1>Potluck Calendar</h1>
-    <calendar-view>
-	    <calendar-view-header/>
-    </calendar-view>
+	<div id="calendar">
+
+		<div class="calendar-parent">
+			<calendar-view
+				:events="events"
+				:show-date="showDate"
+				:time-format-options="{hour: 'numeric', minute:'2-digit'}"
+				:enable-drag-drop="true"
+				:disable-past="disablePast"
+				:disable-future="disableFuture"
+				:show-event-times="showEventTimes"
+				:display-period-uom="displayPeriodUom"
+				:display-period-count="displayPeriodCount"
+				:starting-day-of-week="startingDayOfWeek"
+				:class="themeClasses"
+				:period-changed-callback="periodChanged"
+				@drop-on-date="onDrop"
+				@click-event="onClickEvent"
+			>
+				<calendar-view-header slot="header" slot-scope="t" :header-props="t.headerProps" @input="setShowDate" />
+			</calendar-view>
+		</div>
 	</div>
 </template>
-
 <script>
-import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
-	// The next two lines are processed by webpack. If you're using the component without webpack compilation,
-	// you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.x
-	// require("vue-simple-calendar/static/css/default.css")
-	// require("vue-simple-calendar/static/css/holidays-us.css")
+// For testing against the published version
+import {
+	CalendarView,
+	CalendarViewHeader,
+	CalendarMathMixin,
+} from "vue-simple-calendar"
+// require("vue-simple-calendar/static/css/default.css")
+// require("vue-simple-calendar/static/css/holidays-us.css")
 
-	export default {
-		name: 'app',
-		data: function() {
-			return { showDate: new Date() }
-		},
-		components: {
-			CalendarView,
-			CalendarViewHeader,
-		},
-		methods: {
-			setShowDate(d) {
-				this.showDate = d;
-			},
+// For live testing while making changes to the component, assumes repo pulled to sister folder
+/*
+import CalendarView from "../../vue-simple-calendar/src/components/CalendarView.vue"
+import CalendarViewHeader from "../../vue-simple-calendar/src/components/CalendarViewHeader.vue"
+import CalendarMathMixin from "../../vue-simple-calendar/src/components/CalendarMathMixin.js"
+require("../../vue-simple-calendar/static/css/default.css")
+require("../../vue-simple-calendar/static/css/holidays-us.css")
+*/
+
+export default {
+	name: "App",
+	components: {
+		CalendarView,
+		CalendarViewHeader,
+	},
+	mixins: [CalendarMathMixin],
+	data() {
+		return {
+      /* Show the current month, and give it some fake events to show */
+      t: {
+        headerProps: {
+          
+        },
+      },
+			showDate: this.thisMonth(1),
+			startingDayOfWeek: 0,
+			disablePast: false,
+			disableFuture: false,
+			displayPeriodUom: "month",
+			displayPeriodCount: 1,
+			showEventTimes: true,
+			newEventTitle: "",
+			newEventStartDate: "",
+			newEventEndDate: "",
+			useDefaultTheme: true,
+			useHolidayTheme: true,
+			events: [
+				{
+					id: "e0",
+					startDate: "2018-01-05",
+				},
+				{
+					id: "e1",
+					startDate: this.thisMonth(15, 18, 30),
+				},
+				{
+					id: "e2",
+					startDate: this.thisMonth(15),
+					title: "Single-day event with a long title",
+				},
+				{
+					id: "e3",
+					startDate: this.thisMonth(7, 9, 25),
+					endDate: this.thisMonth(10, 16, 30),
+					title: "Multi-day event with a long title and times",
+				},
+				{
+					id: "e4",
+					startDate: this.thisMonth(20),
+					title: "My Birthday!",
+					classes: "birthday",
+					url: "https://en.wikipedia.org/wiki/Birthday",
+				},
+				{
+					id: "e5",
+					startDate: this.thisMonth(5),
+					endDate: this.thisMonth(12),
+					title: "Multi-day event",
+					classes: "purple",
+				},
+				{
+					id: "foo",
+					startDate: this.thisMonth(29),
+					title: "Same day 1",
+				},
+				{
+					id: "e6",
+					startDate: this.thisMonth(29),
+					title: "Same day 2",
+					classes: "orange",
+				},
+				{
+					id: "e7",
+					startDate: this.thisMonth(29),
+					title: "Same day 3",
+				},
+				{
+					id: "e8",
+					startDate: this.thisMonth(29),
+					title: "Same day 4",
+					classes: "orange",
+				},
+				{
+					id: "e9",
+					startDate: this.thisMonth(29),
+					title: "Same day 5",
+				},
+				{
+					id: "e10",
+					startDate: this.thisMonth(29),
+					title: "Same day 6",
+					classes: "orange",
+				},
+				{
+					id: "e11",
+					startDate: this.thisMonth(29),
+					title: "Same day 7",
+				},
+			],
 		}
-	}
+	},
+	computed: {
+		userLocale() {
+			return this.getDefaultBrowserLocale
+		},
+		dayNames() {
+			return this.getFormattedWeekdayNames(this.userLocale, "long", 0)
+		},
+		themeClasses() {
+			return {
+				"theme-default": this.useDefaultTheme,
+				"holiday-us-traditional": this.useHolidayTheme,
+				"holiday-us-official": this.useHolidayTheme,
+			}
+		},
+	},
+	mounted() {
+		this.newEventStartDate = this.isoYearMonthDay(this.today())
+		this.newEventEndDate = this.isoYearMonthDay(this.today())
+	},
+
+	methods: {
+		periodChanged(range, eventSource) {
+			// Demo does nothing with this information, just including the method to demonstrate how
+			// you can listen for changes to the displayed range and react to them (by loading events, etc.)
+			console.log(eventSource)
+			console.log(range)
+		},
+		thisMonth(d, h, m) {
+			const t = new Date()
+			return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
+		},
+		onClickEvent(e) {
+			this.message = `You clicked: ${e.title}`
+		},
+		setShowDate(d) {
+			this.message = `Changing calendar view to ${d.toLocaleDateString()}`
+			this.showDate = d
+		},
+		onDrop(event, date) {
+			this.message = `You dropped ${event.id} on ${date.toLocaleDateString()}`
+			// Determine the delta between the old start date and the date chosen,
+			// and apply that delta to both the start and end date to move the event.
+			const eLength = this.dayDiff(event.startDate, date)
+			event.originalEvent.startDate = this.addDays(event.startDate, eLength)
+			event.originalEvent.endDate = this.addDays(event.endDate, eLength)
+		},
+		clickTestAddEvent() {
+			this.events.push({
+				startDate: this.newEventStartDate,
+				endDate: this.newEventEndDate,
+				title: this.newEventTitle,
+			})
+			this.message = "You added an event!"
+		},
+	},
+}
 </script>
 
 <style>
-  #calendar {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		color: #2c3e50;
-		height: 67vh;
-		width: 90vw;
-		margin-left: auto;
-		margin-right: auto;
-	}
+html,
+body {
+	height: 100%;
+	margin: 0;
+	background-color: #f7fcff;
+}
+
+#calendar {
+	display: flex;
+	flex-direction: row;
+	font-family: Calibri, sans-serif;
+	width: 95vw;
+	min-width: 30rem;
+	max-width: 50rem;
+	min-height: 40rem;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+.calendar-parent {
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	overflow-x: hidden;
+	overflow-y: hidden;
+	max-height: 80vh;
+	background-color: white;
+}
+
+/* For long calendars, ensure each week gets sufficient height. The body of the calendar will scroll if needed */
+.cv-wrapper.period-month.periodCount-2 .cv-week,
+.cv-wrapper.period-month.periodCount-3 .cv-week,
+.cv-wrapper.period-year .cv-week {
+	min-height: 6rem;
+}
+
+/* These styles are optional, to illustrate the flexbility of styling the calendar purely with CSS. */
+
+/* Add some styling for events tagged with the "birthday" class */
+.calendar .event.birthday {
+	background-color: #e0f0e0;
+	border-color: #d7e7d7;
+}
+
+.calendar .event.birthday::before {
+	content: "\1F382";
+	margin-right: 0.5em;
+}
 </style>
